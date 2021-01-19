77fa3cd20380c1e67a7ac80e5ea3db0024b561d3
Ethan <ebmrules@gmail.com>
2021-01-19 00:15:48 -0800

    added new login / register, as well as password encryption and decryption

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -8,53 +8,78 @@ import { Button, Modal, Form } from "react-bootstrap";
 import Axios from "axios";
 
 
+Axios.defaults.withCredentials = true
+
 
 export default class Home extends Component {
+  
     constructor(props) {
       super(props);
       this.state = {
          show: false,
          loginInfo: {
            username: "",
+           displayName: "",
            password: ""
          },
          isAutheticated: false
       };
     }
 
+    
+    
+
     handleClose = () => this.setState({show:false});
     handleShow = () => this.setState({show:true});
 
     setUsername = (username) => this.setState({loginInfo: {...this.state.loginInfo, username: username }});
     setPassword = (password) => this.setState({loginInfo:  {...this.state.loginInfo, password: password }});
+    setDisplayName = (displayName) => this.setState({loginInfo:  {...this.state.loginInfo, displayName: displayName }});
+    
+    attemptRegister = async () => {
+      console.log(this.state);
+
+      Axios.post("http://localhost:3000/register", {
+        username: this.state.loginInfo.username,
+        displayName: this.state.loginInfo.displayName,
+        password: this.state.loginInfo.password
+      }).then((res) =>
+      {
+        alert(res);
+
+      })
+    }
+   
 
     attemptLogin = async () => {
       console.log(this.state);
 
-      Axios.post("http://localhost:3000/api/auth",{
+      Axios.post("http://localhost:3000/login",{
         username: this.state.loginInfo.username,
         password: this.state.loginInfo.password
       }).then((res) =>
       {
-        this.setState({isAutheticated: res.data});
-        if (this.state.isAutheticated) {
-          console.log(this.state.isAutheticated);
-        } else {
-          console.log(this.state.isAutheticated);
-        }
-      })
 
+        alert(res);
+        // this.setState({isAutheticated: res.data});
+        // if (this.state.isAutheticated) {
+        //   console.log(this.state.isAutheticated);
+        // } else {
+        //   console.log(this.state.isAutheticated);
+        // }
+      });
 
-      //make api call
-    }
-
+    };
+    
     render(){
+
+     
         return (
 
                 <div className="Home">
                         <div className="topDiv">
                             <h1 className="header">TweedleDum</h1>
-                            <h1 className="right">Have an account? <Link className="link" to = "/login">Log in.</Link></h1>
+                            {/* <h1 className="right">Have an account? <Link className="link" to = "/login">Log in.</Link></h1> */}
                             <Button variant="primary" onClick={this.handleShow}> Login </Button>
                             <Modal show={this.state.show} onHide={this.handleClose}>
                               <Modal.Header closeButton>
@@ -70,6 +95,7 @@ export default class Home extends Component {
                                     <Form.Label>Password</Form.Label>
                                     <Form.Control type="password" placeholder="Password" onChange={(e) => {this.setPassword(e.target.value)}} />
                                   </Form.Group>
+                                  
                                 </Form>
                               </Modal.Body>
                               <Modal.Footer>
@@ -77,7 +103,33 @@ export default class Home extends Component {
                                 <Button variant="primary" onClick={this.attemptLogin}>Login</Button>
                               </Modal.Footer>
                             </Modal>
-
+{/* 
+                            <Button variant="secondary" onClick={this.handleShow}> Register </Button>
+                            <Modal show={this.state.show} onHide={this.handleClose}>
+                              <Modal.Header closeButton>
+                                <Modal.Title>Register</Modal.Title>
+                              </Modal.Header>
+                              <Modal.Body>
+                                <Form action="auth" method="POST">
+                                  <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control type="username" placeholder="Enter username" onChange={(e) => {this.setUsername(e.target.value)}}/>
+                                  </Form.Group>
+                                  <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Display Name</Form.Label>
+                                    <Form.Control type="displayname" placeholder="displayName" onChange={(e) => {this.setDisplayName(e.target.value)}} />
+                                  </Form.Group>
+                                  <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" onChange={(e) => {this.setPassword(e.target.value)}} />
+                                  </Form.Group>
+                                </Form>
+                              </Modal.Body>
+                              <Modal.Footer>
+                                <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+                                <Button variant="primary" onClick={this.attemptRegister}>Register</Button>
+                              </Modal.Footer>
+                            </Modal> */}
                         </div>
                         <div className="leftnavBar">
                             <nav>
--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const app = express();
 const cors = require("cors");
 const connection = require('./database');
 const bodyParser = require('body-parser');
-var session = require('express-session');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 
 // app.route('/books/:userId')
@@ -21,44 +25,116 @@ var session = require('express-session');
 //     );
 //   });
 
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
+
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true
 }));
 
-app.use(cors());
+app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
+app.use(session({
+  key: "userId",
+  secret: "etahgamesyoutube",
+  resave: false,
+  saveUninitialized:false,
+  cookie: {
+    //this represents how long the session will be active, in this case it is 24 hours
+    expires: 60 * 60 * 24
+  },
+}));
 
-app.post("/api/auth",  (req, res) => {
+// app.post("/api/auth",  (req, res) => {
 
-  console.log(req.body);
+//   console.log(req.body);
+
+//   const username = req.body.username;
+//   const password = req.body.password;
+//   console.log(username + password);
+//   var isauthenticated = false;
+//   const sqlChecker = "SELECT * "+
+//                       "FROM users "+
+//                       "WHERE uniqueName = ? AND password = ?";
+
+//   connection.query(sqlChecker, [username, password], function (err, result) {
+//     if(err) res.send(err);
+//     else {
+//       const isAuthenticated = (result.length > 0)?true:false;
+//       if (isAuthenticated) {
+//         req.session.loggedIn = true;
+//         req.session.username = username;
+//         res.redirect("/");
+//       }
+//       //res.send(isAuthenticated);
+//     }
+//   });
+
+// })
+
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const displayName = req.body.displayName;
+  const password = req.body.password;
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    connection.query(
+      "INSERT INTO users(uniqueName, publicName, password) VALUES (?,?,?)", 
+      //send hashed password to database for data security
+      [username,displayName,hash],
+      (err, result) => {
+        console.log(err)
+      } )
+  })
+
+  
+})
+
+app.get("/login", (req, res) => {
+  if(req.session.user)
+  {
+    res.send({loggedIn: true, user: req.session.user})
+  } else{
+    res.send({loggedIn: false})
+  }
+})
+
+app.post("/login", (req, res) => {
 
   const username = req.body.username;
   const password = req.body.password;
-  console.log(username + password);
-  var isauthenticated = false;
-  const sqlChecker = "SELECT * "+
-                      "FROM users "+
-                      "WHERE uniqueName = ? AND password = ?";
 
-  connection.query(sqlChecker, [username, password], function (err, result) {
-    if(err) console.log(err);
-    else {
-      const isAuthenticated = (result.length > 0)?true:false;
-      if (isAuthenticated) {
-        req.session.loggedIn = true;
-        req.session.username = username;
-        //res.redirect("/");
+  connection.query(
+    "SELECT * FROM users WHERE uniqueName = ?",
+    [username],
+    (err, result) => {
+      if(err) {
+        res.send({ err: err});
       }
-      res.send(isAuthenticated);
-    }
-  });
 
-})
+      if(result.length > 0){
+        //compare hashed password to the password given for decrypting
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if(response){
+            req.session.user = result;
+            console.log(req.session.user);
+            res.send(result)
+          } else{
+            res.send({message: "Wrong username or password!"})
+          }
+        });
+      } else {
+        res.send({message: "User doesn't exist"});
+      }
+    }
+  )
+
+});
 
 
 
